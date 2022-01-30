cba5646fdde4ef137d0e9056b84f8459a1a1ddc7
Krithi Ramani <krithi.ramani@gmail.com>
2022-01-30 18:02:00 +0530

    added script to populate and select column names in QI parameter doctype

--- vesta_si_erpnext/hooks.py
+++ vesta_si_erpnext/hooks.py
@@ -35,7 +35,8 @@ app_license = "MIT"
 # include js in doctype views
 doctype_js = {
 	"Quality Inspection" : "public/js/quality_inspection.js",
-	"Stock Entry": "public/js/stock_entry.js"
+	"Stock Entry": "public/js/stock_entry.js",
+	"Quality Inspection Parameter": "public/js/quality_inspection_parameter.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
