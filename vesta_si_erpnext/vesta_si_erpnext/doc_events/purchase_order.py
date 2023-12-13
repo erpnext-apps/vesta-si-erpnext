fe4b269483d1b45e7f5492c900effbb91d4f6614
ViralKansodiya-Fosserp <141210323+viralkansodiya@users.noreply.github.com>
2023-12-13 17:33:21 +0530

    on validate time added on state aproval

---
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_order.py
@@ -0,0 +1,17 @@
+import frappe
+from frappe.utils import now
+from datetime import datetime
+
+def validate(self , method):
+    if not self.custom_level_1_approval_pending and self.workflow_state =="Level 1 Approval Pending":
+        self.custom_level_1_approval_pending = now()
+    if not self.custom_level_2_approval_pending and self.workflow_state == "Level 2 Approval Pending":
+        self.custom_level_2_approval_pending = now()
+    if not self.custom_approved and self.workflow_state == "Approved":
+        self.custom_approved = now()
+    if not self.custom_approved_and_reviewed and self.workflow_state == "Approved and Reviewed":
+        self.custom_approved_and_reviewed = now()
+    if not self.custom_rejected and self.workflow_state == "Rejected":
+        self.custom_rejected = now()
+    
+    
