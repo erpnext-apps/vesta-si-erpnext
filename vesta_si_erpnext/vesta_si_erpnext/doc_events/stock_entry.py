fd34747627fd3adc1c7acd1a498ff6422c43a070
viral <viral@fosserp.com>
2024-04-12 07:11:39 +0000

    new report and stock entry validation

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/stock_entry.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/stock_entry.py
@@ -3,10 +3,13 @@ from frappe import _
 
 def validate_manufactured_row_material(self, method):
     error_data = []
+    missing_data = []
     if self.stock_entry_type in ["Manufacture",'Repack']:
         for row in self.items:
-            if (row.is_finished_item == 0 or row.is_scrap_item) and not row.s_warehouse:
+            if (row.is_finished_item == 0 or row.is_scrap_item == 0) and not row.s_warehouse:
                 error_data.append(row)
+            if not row.s_warehouse and not (row.is_finished_item and row.is_scrap_item):
+                frappe.throw(f"Row #{row.idx}:- Please enable <b style='color:red;'>Is Finished Item</b> or <b style='color:red;'>Is Scrap Item</b> when source warehouse left blank")
 
     if error_data:
         message  = '<b>Note: Please specify the source warehouse for the raw materials listed below.</b> <br><br>'
