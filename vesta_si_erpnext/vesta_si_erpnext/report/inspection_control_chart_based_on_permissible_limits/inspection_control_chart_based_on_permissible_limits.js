923505e8748d04a10a1d44bf49e12d956739e041
viral <viral@fosserp.com>
2024-04-29 10:20:51 +0000

    New Report

---
+++ vesta_si_erpnext/vesta_si_erpnext/report/inspection_control_chart_based_on_permissible_limits/inspection_control_chart_based_on_permissible_limits.js
@@ -0,0 +1,62 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Inspection Control Chart Based On Permissible Limits"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+			"fieldname":"item_code",
+			"label": __("Item Code"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"get_query": function() {
+				return {
+					filters: {
+						"has_batch_no": 1
+					}
+				};
+			},
+			"reqd":1
+		},
+		{
+			"fieldname":"qi_parameter",
+			"label": __("QI Parameter"),
+			"fieldtype": "Link",
+			"options": "Quality Inspection Parameter",
+		}
+	],
+	formatter: function (value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (!(data['max_value'] >= data['reading_1'] >= data['min_value'])){
+			console.log(column)
+			value = value.replace('<a ', '<a style="color: red;" ')
+			value = "<span style='color:red'>" + value + "</span>";
+		}
+		
+		return value
+	}
+};
