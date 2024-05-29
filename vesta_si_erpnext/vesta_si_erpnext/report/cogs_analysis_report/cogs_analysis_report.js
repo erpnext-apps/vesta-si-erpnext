64d4f11dc4b5ec3a27ebad748613899645622476
root <viral@fosserp.com>
2024-05-29 05:56:36 +0000

    Add Total and added a options in filter

--- vesta_si_erpnext/vesta_si_erpnext/report/cogs_analysis_report/cogs_analysis_report.js
+++ vesta_si_erpnext/vesta_si_erpnext/report/cogs_analysis_report/cogs_analysis_report.js
@@ -25,7 +25,7 @@ frappe.query_reports["COGS Analysis Report"] = {
 			"label":__("SKF/Non-SKF/Both"),
 			"fieldtype":"Select",
 			"width": "80",
-			"options":["","SKF","Non-SKF"]
+			"options":["SKF","Non-SKF","Both"]
 		}
 	]
 };
