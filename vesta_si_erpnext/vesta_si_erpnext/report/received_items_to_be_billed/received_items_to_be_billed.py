2c2e379423e02241cc30ca0da63d9bb44d68fce3
viral <viral@fosserp.com>
2024-06-18 12:54:54 +0000

    changes

--- vesta_si_erpnext/vesta_si_erpnext/report/received_items_to_be_billed/received_items_to_be_billed.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
-# For license information, please see license.txt
-
-# import frappe
-
-
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
--- vesta_si_erpnext/vesta_si_erpnext/report/received_items_to_be_billed___vesta_si/received_items_to_be_billed___vesta_si.py
+++ vesta_si_erpnext/vesta_si_erpnext/report/received_items_to_be_billed___vesta_si/received_items_to_be_billed___vesta_si.py
@@ -8,7 +8,11 @@ def execute(filters=None):
 	columns = get_column()
 	args = get_args()
 	data = get_ordered_to_be_billed_data(args, filters)
-	return columns, data
+	final_data = []
+	for row in data:
+		if row[-1] == filters.get('account'):
+			final_data.append(row)
+	return columns, final_data
 
 
 def get_column():
@@ -80,6 +84,13 @@ def get_column():
 			"options": "Company",
 			"width": 120,
 		},
+		{
+			"label": _("Expense Account"),
+			"fieldname": "expense_account",
+			"fieldtype": "Link",
+			"options": "Account",
+			"width": 120,
+		},
 	]
 
 
@@ -109,12 +120,11 @@ def get_ordered_to_be_billed_data(args, filters):
 	)
 
 	project_field = get_project_field(doctype, party)
-	cond = ''
-	if filters.get('account'):
-		cond = "and acc.account_type = '{account}'".format(account=filters.get('account'))
+
 	return frappe.db.sql(
 		"""
 		Select
+			
 			`{parent_tab}`.name, `{parent_tab}`.{date_field},
 			`{parent_tab}`.{party}, `{parent_tab}`.{party}_name,
 			`{child_tab}`.item_code,
@@ -125,13 +135,12 @@ def get_ordered_to_be_billed_data(args, filters):
 			(`{child_tab}`.billed_amt * ifnull(`{parent_tab}`.conversion_rate, 1)) -
 			(`{child_tab}`.base_rate * ifnull(`{child_tab}`.returned_qty, 0))),
 			`{child_tab}`.item_name, `{child_tab}`.description,
-			{project_field}, `{parent_tab}`.company
+			{project_field}, `{parent_tab}`.company,
+			`{child_tab}`.expense_account
 		from
-			`{parent_tab}`
-			Left Join `{child_tab}` ON  `{child_tab}`.parent = `{parent_tab}`.name
-			Left join `tabAccount` as acc ON acc.name = `{child_tab}`.expense_account
+			`{parent_tab}`, `{child_tab}` 
 		where
-			`{parent_tab}`.name = `{child_tab}`.parent and `{parent_tab}`.docstatus = 1 {cond}
+			`{parent_tab}`.name = `{child_tab}`.parent and `{parent_tab}`.docstatus = 1
 			and `{parent_tab}`.status not in ('Closed', 'Completed')
 			and `{child_tab}`.amount > 0
 			and (`{child_tab}`.base_amount -
@@ -150,8 +159,7 @@ def get_ordered_to_be_billed_data(args, filters):
 			order=args.get("order"),
 			order_by=args.get("order_by"),
 			from_date = filters.get('from_date'),
-			to_date = filters.get('to_date'),
-			cond = cond
+			to_date = filters.get('to_date')
 		)
 	)
 
