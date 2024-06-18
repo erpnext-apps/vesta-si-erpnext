c3387e3b73c875950b03c36d09276801ee5c9030
viral <viral@fosserp.com>
2024-06-18 11:30:20 +0000

    Api for dashboard

--- vesta_si_erpnext/api.py
+++ vesta_si_erpnext/api.py
@@ -1,6 +1,8 @@
 import frappe
 import json
-
+from frappe.utils import nowdate, getdate
+import datetime
+import calendar
 
 def install_pandas():
     try:
@@ -14,16 +16,21 @@ def install_pandas():
             print("Pandas has been successfully installed.")
         except Exception as e:
             print("Error occurred while installing Pandas:", e)
+
 @frappe.whitelist()
 def get_penalty_cost_paid_suppliers(filters):
+    from erpnext.accounts.utils import get_fiscal_year
+    current_year = get_fiscal_year(nowdate(),as_dict =1)
+
+    from_date = str(current_year.year_start_date)
+    to_date = str(current_year.year_end_date)
+
     filters = json.loads(filters)
     cond = ''
     if filters.get('expense_account'):
         cond += f" and pii.expense_account = '{filters.get('expense_account')}'"
-    if filters.get('from_date'):
-        cond += f" and pi.posting_date >= '{filters.get('from_date')}'"
-    if filters.get('to_date'):
-        cond += f" and pi.posting_date <= '{filters.get('to_date')}'"
+    cond += f" and pi.posting_date >= '{from_date}'"
+    cond += f" and pi.posting_date <= '{to_date}'"
     data = frappe.db.sql(f"""
             Select count(pi.supplier) as number_of_supplier
             from `tabPurchase Invoice` as pi
@@ -34,5 +41,62 @@ def get_penalty_cost_paid_suppliers(filters):
     
     return {
                 "value": len(data),
+                "fieldtype": "Float",
+            }
+
+@frappe.whitelist()
+def get_total_penalty_cost_remitted_to_suppliers(filters):
+    filters = json.loads(filters)
+
+    from erpnext.accounts.utils import get_fiscal_year
+    current_year = get_fiscal_year(nowdate(),as_dict =1)
+
+    from_date = str(current_year.year_start_date)
+    to_date = str(current_year.year_end_date)
+
+    cond = ''
+    if filters.get('company'):
+        cond += f" and pi.company = '{filters.get('company')}'"
+    if filters.get('expense_account'):
+        cond += f" and pii.expense_account = '{filters.get('expense_account')}'"
+    
+    cond += f" and pi.posting_date >= '{from_date}'"
+
+    cond += f" and pi.posting_date <= '{to_date}'"
+    data = frappe.db.sql(f"""
+            Select sum(pii.base_net_amount) as base_net_amount, pi.name, pii.expense_account
+            From `tabPurchase Invoice` as pi
+            left Join `tabPurchase Invoice Item` as pii ON pii.parent = pi.name
+            Where pi.docstatus = 1 {cond}
+    """,as_dict = 1)
+    return {
+                "value": data[0].get('base_net_amount') if data else 0,
+                "fieldtype": "Float",
+            }
+
+@frappe.whitelist()
+def get_total_penalty_cost_remitted_to_suppliers_monthly(filters):
+    filters = json.loads(filters)
+    current_date = getdate()
+    from_date = current_date.replace(day=1)
+    to_date = current_date.replace(day=calendar.monthrange(current_date.year, current_date.month)[1])
+
+    cond = ''
+    if filters.get('company'):
+        cond += f" and pi.company = '{filters.get('company')}'"
+    if filters.get('expense_account'):
+        cond += f" and pii.expense_account = '{filters.get('expense_account')}'"
+   
+    cond += f" and pi.posting_date >= '{from_date}'"
+
+    cond += f" and pi.posting_date <= '{to_date}'"
+    data = frappe.db.sql(f"""
+            Select sum(pii.base_net_amount) as base_net_amount, pi.name
+            From `tabPurchase Invoice` as pi
+            left Join `tabPurchase Invoice Item` as pii ON pii.parent = pi.name
+            Where pi.docstatus = 1 {cond}
+    """,as_dict = 1)
+    return {
+                "value": data[0].get('base_net_amount') if data and data[0].get('base_net_amount') else 0,
                 "fieldtype": "Float",
             }
