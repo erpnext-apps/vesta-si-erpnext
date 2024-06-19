8fdd6d20d40a0e177a59334adba1c47119cfaaa7
root <viral@fosserp.com>
2024-06-19 13:48:37 +0000

    remove payment validation

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
@@ -24,26 +24,27 @@ def set_due_date_after_submit(self, method):
 			self.reload()
 
 def validate(self, method):
-	get_advance_entries(self)
-	party_account_currency = frappe.db.get_value("Account", self.credit_to, 'account_currency')
-	company_currency = frappe.db.get_value("Company", self.company, 'default_currency')
-	data = get_negative_outstanding_invoices(
-				"Supplier", 
-				self.supplier, 
-				self.credit_to, 
-				party_account_currency, 
-				company_currency,
-				condition = '')
-	if len(data):
-		for row in data:
-			message = "Debit Note and Payment Entry available against this supplier <b>{0}</b><br>".format(get_link_to_form("Supplier",self.supplier))
-			message +="First reconcile those entry, reference available as mentioned below"
-			message += "<br><br>"
-			message += """<table width='100%'>"""
-			for row in data:
-				message += "<tr><td>{0}</td><td>{1} {2}</td></tr>".format(get_link_to_form(row.voucher_type, row.voucher_no),self.currency, row.outstanding_amount)
-			message += "</table>"
-			frappe.throw(message)
+	pass
+	# get_advance_entries(self)
+	# party_account_currency = frappe.db.get_value("Account", self.credit_to, 'account_currency')
+	# company_currency = frappe.db.get_value("Company", self.company, 'default_currency')
+	# data = get_negative_outstanding_invoices(
+	# 			"Supplier", 
+	# 			self.supplier, 
+	# 			self.credit_to, 
+	# 			party_account_currency, 
+	# 			company_currency,
+	# 			condition = '')
+	# if len(data):
+	# 	for row in data:
+	# 		message = "Debit Note and Payment Entry available against this supplier <b>{0}</b><br>".format(get_link_to_form("Supplier",self.supplier))
+	# 		message +="First reconcile those entry, reference available as mentioned below"
+	# 		message += "<br><br>"
+	# 		message += """<table width='100%'>"""
+	# 		for row in data:
+	# 			message += "<tr><td>{0}</td><td>{1} {2}</td></tr>".format(get_link_to_form(row.voucher_type, row.voucher_no),self.currency, row.outstanding_amount)
+	# 		message += "</table>"
+	# 		frappe.throw(message)
 
 def get_advance_entries(self):
 	res = self.get_advance_entries(
