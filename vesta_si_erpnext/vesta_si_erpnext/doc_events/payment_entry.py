974a839e321eac5ef4c40d66bc903b5b6abc0380
viral <viral@fosserp.com>
2024-06-19 09:56:04 +0000

    stoping if advance amount paid

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/payment_entry.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/payment_entry.py
@@ -1,5 +1,20 @@
 import frappe
-from erpnext.accounts.utils import get_outstanding_invoices
+from erpnext.accounts.doctype.payment_entry.payment_entry import get_negative_outstanding_invoices
+from frappe.utils import get_link_to_form, comma_and, flt
+from frappe.utils import (
+	add_days,
+	add_months,
+	cint,
+	comma_and,
+	flt,
+	fmt_money,
+	formatdate,
+	get_last_day,
+	get_link_to_form,
+	getdate,
+	nowdate,
+	today,
+)
 
 def validate(self, method):
     currency_list = []    
@@ -9,12 +24,42 @@ def validate(self, method):
 
         if len(list(set(currency_list))) > 1:
             frappe.throw("Purchase Invoices have different currencies. All selected purchase invoices must have the same currency.")
-        elif self.paid_from_account_currency != list(set(currency_list))[0]:
+        elif list(set(currency_list)) and self.paid_from_account_currency != list(set(currency_list))[0]:
             frappe.throw(f"Account Paid From should be in <b>{list(set(currency_list))[0]}<b>")
-        # if self.payment_type == "Pay":
-        #     data = get_outstanding_invoices(party_type="Supplier", party = self.party)
-        #     frappe.throw(str(data))
+        party_account_currency = frappe.db.get_value("Account", self.paid_to, 'account_currency')
+        
+        company_currency = frappe.db.get_value("Company", self.company, 'default_currency')
+        
+        if self.payment_type == "Pay" and self.party_type == "Supplier":
+            data = get_negative_outstanding_invoices(
+                    "Supplier", 
+                    self.party, 
+                    self.paid_to, 
+                    party_account_currency, 
+                    company_currency,
+                    condition = '')
+            if len(data):
+                for row in data:
+                    message = "Debit Note and Payment Entry available against this supplier {0}<br>".format(get_link_to_form(self.party_type, self.party))
+                    message +="First reconcile those entry, reference available as mentioned below"
+                    message += "<br><br>"
+                    message += """<table width='100%'>"""
+                    for row in data:
+                        message += "<tr><td>{0}</td><td>{1} {2}</td></tr>".format(get_link_to_form(row.voucher_type, row.voucher_no),self.paid_to_account_currency, row.outstanding_amount)
+                    message += "</table>"
+                    frappe.throw(message)
+        if len(self.references):
+            if self.references[0].reference_doctype == "Purchase Invoice":
+                doc = frappe.get_doc("Purchase Invoice", self.references[0].reference_name)
+                get_advance_entries(doc)
 
+def get_advance_entries(self):
+	res = self.get_advance_entries(
+			include_unallocated=not cint(self.get("only_include_allocated_payments"))
+		)
+	if res and not self.allocate_advances_automatically:
+		if not len(self.advances):
+			frappe.throw("Advance payments available against supplier <b>{0}</b> <br> Enable <b>'Set Advances and Allocate (FIFO)'</b> or click on the <b>'Get Advances Paid'</b> button under the payments section.".format(self.supplier))
 
 def on_submit(self, method):
     data =  frappe.db.sql(f'''
--- vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
@@ -1,6 +1,54 @@
 import frappe
+from frappe.utils import get_link_to_form, comma_and, flt
+from erpnext.accounts.doctype.payment_entry.payment_entry import get_negative_outstanding_invoices
+from frappe.utils import (
+	add_days,
+	add_months,
+	cint,
+	comma_and,
+	flt,
+	fmt_money,
+	formatdate,
+	get_last_day,
+	get_link_to_form,
+	getdate,
+	nowdate,
+	today,
+)
+
 
 def set_due_date_after_submit(self, method):
-    if self.docstatus == 1:
-        frappe.db.set_value("Purchase Invoice", self.name, 'due_date', self.payment_schedule[-1].due_date)
-        self.reload()
+	if self.docstatus == 1:
+		if self.payment_schedule:
+			frappe.db.set_value("Purchase Invoice", self.name, 'due_date', self.payment_schedule[-1].due_date)
+			self.reload()
+
+def validate(self, method):
+	get_advance_entries(self)
+	party_account_currency = frappe.db.get_value("Account", self.credit_to, 'account_currency')
+	company_currency = frappe.db.get_value("Company", self.company, 'default_currency')
+	data = get_negative_outstanding_invoices(
+				"Supplier", 
+				self.supplier, 
+				self.credit_to, 
+				party_account_currency, 
+				company_currency,
+				condition = '')
+	if len(data):
+		for row in data:
+			message = "Debit Note and Payment Entry available against this supplier <b>{0}</b><br>".format(get_link_to_form("Supplier",self.supplier))
+			message +="First reconcile those entry, reference available as mentioned below"
+			message += "<br><br>"
+			message += """<table width='100%'>"""
+			for row in data:
+				message += "<tr><td>{0}</td><td>{1} {2}</td></tr>".format(get_link_to_form(row.voucher_type, row.voucher_no),self.currency, row.outstanding_amount)
+			message += "</table>"
+			frappe.throw(message)
+
+def get_advance_entries(self):
+	res = self.get_advance_entries(
+			include_unallocated=not cint(self.get("only_include_allocated_payments"))
+		)
+	if res and not self.allocate_advances_automatically:
+		if not len(self.advances):
+			frappe.throw("Advance payments available against supplier <b>{0}</b> <br> Enable <b>'Set Advances and Allocate (FIFO)'</b> or click on the <b>'Get Advances Paid'</b> button under the payments section.".format(self.supplier))
