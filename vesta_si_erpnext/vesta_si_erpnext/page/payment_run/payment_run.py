cf4242731c9038a9b9e023365ba369e1918f5261
viral <viral@fosserp.com>
2024-06-28 05:43:11 +0000

    ch

--- vesta_si_erpnext/vesta_si_erpnext/page/payment_run/payment_run.py
+++ vesta_si_erpnext/vesta_si_erpnext/page/payment_run/payment_run.py
@@ -126,7 +126,8 @@ def get_payment_entry(
 		if party_bank_account:
 			account = frappe.db.get_value("Bank Account", party_bank_account, "account")
 			bank = get_bank_cash_account(doc, account)
-
+	
+	bank = frappe.get_doc("Account",account_paid_from)
 	paid_amount, received_amount = set_paid_amount_and_received_amount(
 		dt, party_account_currency, bank, outstanding_amount, payment_type, bank_amount, doc
 	)
