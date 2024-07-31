1212de222f90fc30fe1de5bd0abe55f3c6fa6ad9
root <viral@fosserp.com>
2024-07-31 08:56:32 +0000

    changes

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
@@ -36,15 +36,15 @@ def validate(self, method):
 				company_currency,
 				condition = '')
 	if len(data):
+		message = "Debit Note and Payment Entry available against this supplier <b>{0}</b><br>".format(get_link_to_form("Supplier",self.supplier))
+		message +="First reconcile those entry, reference available as mentioned below"
+		message += "<br><br>"
+		message += """<table width='100%'>"""
 		for row in data:
-			message = "Debit Note and Payment Entry available against this supplier <b>{0}</b><br>".format(get_link_to_form("Supplier",self.supplier))
-			message +="First reconcile those entry, reference available as mentioned below"
-			message += "<br><br>"
-			message += """<table width='100%'>"""
-			for row in data:
-				message += "<tr><td>{0}</td><td>{1} {2}</td></tr>".format(get_link_to_form(row.voucher_type, row.voucher_no),self.currency, row.outstanding_amount)
-			message += "</table>"
-			frappe.msgprint(message)
+			message += "<tr><td>{0}</td><td>{1} {2}</td></tr>".format(get_link_to_form(row.voucher_type, row.voucher_no),self.currency, row.outstanding_amount)
+		message += "</table>"
+		
+		frappe.msgprint(message)
 
 def get_advance_entries(self):
 	res = self.get_advance_entries(
