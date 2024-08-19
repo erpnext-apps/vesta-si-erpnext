c8daffe97252538f12cecdadac627b95b127675a
root <viral@fosserp.com>
2024-08-19 11:21:54 +0000

    ch

--- vesta_si_erpnext/vesta_si_erpnext/page/payment_export/payment_export.py
+++ vesta_si_erpnext/vesta_si_erpnext/page/payment_export/payment_export.py
@@ -738,6 +738,9 @@ def gen_payment_export_log(content, total_no_of_payments, total_paid_amount, pay
             data_["Supplier Name"] = frappe.db.get_value("Supplier", row.get('supplier'), 'supplier_name')
             data_["Paid Amount"] = row.get('paid_amount')
             data_["Currency"] = frappe.db.get_value("Payment Entry", row.get('payment_entry'), "paid_to_account_currency")
+            data_["document_type"] = row.get('document_type')
+            data_["Purchase Invoice"] = row.get('purchase_doc_no')
+            data_["Account"] = row.get('account')
             json_data.append(data_)
         # Json to DataFrame
         df = pd.DataFrame(json_data)
