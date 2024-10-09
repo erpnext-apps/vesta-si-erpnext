0f936e44c52b0a87eeaef03a29629d531c6c5ad1
root <viral@fosserp.com>
2024-10-09 09:37:25 +0000

    ch'

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
@@ -96,6 +96,6 @@ def get_advance_entries(self):
 # 				))
 
 def on_submit(self, method):
-	if  self.advances and self.outstanding_amount:
+	if self.advances and self.outstanding_amount:
 		frappe.msgprint("The amount outstanding is <b>{0}</b>cd  Euro, resulting from the currency exchange variance between the Payment date and the Purchase Invoice date. Please proceed with creating a journal entry to offset this outstanding amount.".format(self.outstanding_amount))
 	
