2f178502476d9646f23776f5a9b0d1dc05c54675
root <viral@fosserp.com>
2024-10-17 06:25:55 +0000

    chreport Monthly Purchadse Payment Analysis

---
+++ vesta_si_erpnext/vesta_si_erpnext/report/monthly_purchase_payment_analysis/monthly_purchase_payment_analysis.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Monthly Purchase Payment Analysis"] = {
+	"filters": [
+		{
+			fieldname:"payment_export_log",
+			label: __("Payment Export Log"),
+			fieldtype: "Link",
+			options : "Payment Export Log",
+			reqd: 0
+		},
+		{
+			fieldname:"from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.month_start(), -1),
+			reqd: 1
+		},
+		{
+			fieldname:"to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_days(frappe.datetime.month_start(),-1),
+			reqd: 1
+		},
+		{
+			fieldname : "delay_payment",
+			label : "Only Delay Payment",
+			fieldtype : "Check",
+			default : 1
+		}
+	]
+};
