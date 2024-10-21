9e707da4194fcdb558644c27b27c6e0f228fde66
root <viral@fosserp.com>
2024-10-21 06:22:56 +0000

    Cancelled payment entry button

--- vesta_si_erpnext/vesta_si_erpnext/doctype/payment_export_log/payment_export_log.js
+++ vesta_si_erpnext/vesta_si_erpnext/doctype/payment_export_log/payment_export_log.js
@@ -6,7 +6,6 @@ frappe.ui.form.on('Payment Export Log', {
 		frm.get_field('logs').grid.cannot_add_rows = true;
 	},
 	refresh: frm => {
-		
 		if(frm.doc.status != 'Submitted' && !frm.doc.__unsaved){
 			frm.add_custom_button(__("Submit Payment Entry"),
 			function () {
@@ -22,5 +21,39 @@ frappe.ui.form.on('Payment Export Log', {
 				},
 			).addClass("btn btn-primary");;
 		}
+		frm.add_custom_button(__('Cancel Payment Entry'), () => {
+			let selected_row = []
+			frm.doc.logs.forEach(e => {
+				if(e.__checked && e.status == "Submitted"){
+					selected_row.push({"payment_entry" : e.payment_entry, "log_ref" : e.name})
+				}
+			});
+			if (!selected_row.length){
+				frappe.throw("Please select at least one row.")
+			}
+			let seconds_elapsed = 0
+			selected_row.forEach(e=>{
+				setup_progress_bar(selected_row, seconds_elapsed)
+			})
+			frm.call({
+				method : "cancelled_payment_entry",
+				args : {
+					"pe" : selected_row,
+					"pe_log" : frm.doc.name
+				},
+				callback:(e)=>{
+					frm.refresh()
+				}
+			})
+		})
 	}
 });
+
+function setup_progress_bar(selected_row, seconds_elapsed) {
+	if (selected_row.length < 5) return;
+
+	let interval = setInterval(function () {
+		seconds_elapsed += 2;
+		frappe.show_progress(__("Cancellation of a Payment Entry"), seconds_elapsed, selected_row.length);
+	}, 1);
+}
