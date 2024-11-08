bf837fac8b107d64762c146a6adf86eeee3f162f
root <viral@fosserp.com>
2024-11-08 04:24:42 +0000

    ch

--- vesta_si_erpnext/hooks.py
+++ vesta_si_erpnext/hooks.py
@@ -123,7 +123,8 @@ doc_events = {
 			"vesta_si_erpnext.vesta_si_erpnext.purchase_receipt.notification_to_assignee"
 		],
 		"before_validate": "vesta_si_erpnext.vesta_si_erpnext.purchase_receipt.before_validate",
-		"on_update": "vesta_si_erpnext.vesta_si_erpnext.purchase_receipt.on_update"
+		"on_update": "vesta_si_erpnext.vesta_si_erpnext.purchase_receipt.on_update",
+		"validate" : "vesta_si_erpnext.vesta_si_erpnext.purchase_receipt.validate"
 	},
 	"Purchase Order":{
 		"validate":"vesta_si_erpnext.vesta_si_erpnext.doc_events.purchase_order.validate"
