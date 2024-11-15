1da1451dd483d91b1d89a1cc71e5d3a00efd2932
root <viral@fosserp.com>
2024-11-15 03:30:52 +0000

    ch

---
+++ vesta_si_erpnext/vesta_si_erpnext/report/pi_on_payment/pi_on_payment.js
@@ -0,0 +1,29 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["PI on Payment"] = {
+	"filters": [
+		{
+			fieldname:"from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.month_start(), -1),
+			reqd: 1
+		},
+		{
+			fieldname:"to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_days(frappe.datetime.month_start(),-1),
+			reqd: 1
+		},
+		{
+			fieldname : "chart_type",
+			label : "Chart Type",
+			fieldtype : "Select",
+			options : ["On Time Payments", "Delayed Payments"],
+			default : "On Time Payments"
+		}
+	]
+};
