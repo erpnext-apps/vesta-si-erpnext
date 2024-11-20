5ca53b9823ece98ddef65da42d535c99c2bc4d31
root <viral@fosserp.com>
2024-11-20 04:16:16 +0000

    ch

---
+++ vesta_si_erpnext/vesta_si_erpnext/report/exchange_rate_analysis_cycle/exchange_rate_analysis_cycle.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Exchange Rate Analysis Cycle"] = {
+	"filters": [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: erpnext.utils.get_fiscal_year(frappe.datetime.get_today(), true)[1],
+			reqd: 1,
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: erpnext.utils.get_fiscal_year(frappe.datetime.get_today(), true)[2],
+			reqd: 1,
+		}
+	]
+};
