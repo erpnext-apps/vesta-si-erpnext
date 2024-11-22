269537fc5476b4feddcb287548a241c05ca0c751
viral <viral@fosserp.com>
2024-11-22 05:17:36 +0000

    ch

--- vesta_si_erpnext/vesta_si_erpnext/report/exchange_rate_analysis_cycle/exchange_rate_analysis_cycle.js
+++ vesta_si_erpnext/vesta_si_erpnext/report/exchange_rate_analysis_cycle/exchange_rate_analysis_cycle.js
@@ -8,14 +8,14 @@ frappe.query_reports["Exchange Rate Analysis Cycle"] = {
 			fieldname: "from_date",
 			label: __("From Date"),
 			fieldtype: "Date",
-			default: frappe.defaults.get_user_default("year_start_date"),
+			default: frappe.defaults.get_user_default("year_start_date"), // change this before version update --> erpnext.utils.get_fiscal_year(frappe.datetime.get_today(), true)[1],
 			reqd: 1,
 		},
 		{
 			fieldname: "to_date",
 			label: __("To Date"),
 			fieldtype: "Date",
-			default: frappe.defaults.get_user_default("year_end_date"),
+			default: frappe.defaults.get_user_default("year_end_date"), // change this before version update --> erpnext.utils.get_fiscal_year(frappe.datetime.get_today(), true)[1],
 			reqd: 1,
 		},
 	]
