384eec53374f8e3d9a771070c6d47088c842af77
root <viral@fosserp.com>
2024-11-25 11:09:49 +0000

    added a status filter multiselect type

--- vesta_si_erpnext/vesta_si_erpnext/report/exchange_rate_analysis_cycle/exchange_rate_analysis_cycle.js
+++ vesta_si_erpnext/vesta_si_erpnext/report/exchange_rate_analysis_cycle/exchange_rate_analysis_cycle.js
@@ -18,5 +18,23 @@ frappe.query_reports["Exchange Rate Analysis Cycle"] = {
 			default: frappe.defaults.get_user_default("year_end_date"), // change this before version update --> erpnext.utils.get_fiscal_year(frappe.datetime.get_today(), true)[1],
 			reqd: 1,
 		},
+		{
+			fieldname: "status",
+			label: __("Status"),
+			fieldtype: "MultiSelectList",
+			options : [
+				{'description' : '', "value":'Draft'},
+				{'description' : '', "value":'Return'},
+				{'description' : '', "value":'Debit Note Issued'},
+				{'description' : '', "value":'Submitted'},
+				{'description' : '', "value":'Paid'},
+				{'description' : '', "value":'Partly Paid'},
+				{'description' : '', "value":'Unpaid'},
+				{'description' : '', "value":'Overdue'},
+				{'description' : '', "value":'Cancelled'},
+				{'description' : '', "value":'Internal Transfer'}
+			],
+			width:200
+		}
 	]
 };
