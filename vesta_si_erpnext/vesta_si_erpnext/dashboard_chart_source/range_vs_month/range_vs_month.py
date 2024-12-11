7e9993c7a454ff5313bc5988c088b522fd0a8931
viral <viral@fosserp.com>
2024-12-11 09:20:55 +0000

    ch

--- vesta_si_erpnext/vesta_si_erpnext/dashboard_chart_source/range_vs_month/range_vs_month.py
+++ vesta_si_erpnext/vesta_si_erpnext/dashboard_chart_source/range_vs_month/range_vs_month.py
@@ -19,12 +19,10 @@ def get_data(filters=None):
     if filters.get("range1"):
         label1 = "{0} - {1}".format(0, filters.get("range1"))
     if filters.get("range2"):
-        label2 = "{0} - {1}".format(filters.get("range1") + 1, filters.get("range1"))
+        label2 = "{0} - {1}".format(filters.get("range1") + 1, filters.get("range2"))
     if filters.get("range3"):
         label3 = "{0} - {1}".format(filters.get("range2") + 1, filters.get("range3"))
     if filters.get("range3"):
-        label4 = "{0} - {1}".format(filters.get("range2") + 1, filters.get("range3"))
-    if filters.get("range4"):
         label4 = "{0} - {1}".format(filters.get("range3") + 1, filters.get("range4"))
     if filters.get("range4"):
         label5 = "{0} - {1}".format("Above", filters.get("range4"))
@@ -35,20 +33,6 @@ def get_data(filters=None):
     january_range4 = 0
     january_range5 = 0
 
-    month_number = {
-        "January" : 1,
-        "February" : 2,
-        "March" : 3,
-        "April" : 4,
-        "May" : 5,
-        "June" : 6,
-        "July" : 7,
-        "August" : 8, 
-        "September" : 9, 
-        "October" : 10,
-        "November" : 11,
-        "December" : 12
-    }
     labels = [
         "January" ,
         "February" ,
