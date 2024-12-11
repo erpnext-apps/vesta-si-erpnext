8bae494cf7a3f46cfcf18a7aa40e198a4f63be9c
viral <viral@fosserp.com>
2024-12-11 13:38:12 +0000

    ch

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/xml_automation.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/xml_automation.py
@@ -4,15 +4,18 @@ from frappe.utils import today
 from frappe.desk.form.load import get_attachments
 from vesta_si_erpnext.vesta_si_erpnext.doc_events.sftp_transfer import Sftp
 
-hostname = "52.157.97.77"
-username = "cm-skftest-12"
-password = "*EklmvpLlzV!.xKt7rTsd7Mh"
-remote_path = "/in/payments/"     # Replace with your remote path
-port = 22
 
 # # Create an instance of the Sftp class
+@frappe.whitelist()
+def get_payment_entry(payments, payment_export_settings, posting_date, payment_type, bank_account = None):
+    # frappe.throw(str(payments))
+    sftp = frappe.get_doc("SFTP Credential")
+    hostname = sftp.hostname
+    username = sftp.username
+    password = sftp.password
+    remote_path = sftp.remote_path    # Replace with your remote path
+    port = 22
 
-def get_payment_entry():
     payment_export_settings = get_payment_export_settings()
     payment_type = [
         "Domestic (Swedish) Payments (SEK)",
@@ -20,45 +23,45 @@ def get_payment_entry():
         "Cross Border Payments (USD)",
         "Cross Border Payments (EUR)",
     ]
-    for row in payment_type:
-        data = get_payments(row, payment_export_settings)
-        payments = data.get("payments")
-        payments_ = []
-        for d in payments:
-            payments_.append(d.get("name"))
-        if payments_:
-            xml_content = generate_payment_file(str(payments_), payment_export_settings, today(), row)
-            content = xml_content.get("content")
-            
-            file_name = "vesta/public/files/payments{0}.xml".format(xml_content.get("time"))
-            try:
-                with open(file_name, "w", encoding="utf-8") as file:
-                    file.write(content)
-                print(f"XML file '{file_name}' generated successfully!")
-            except Exception as e:
-                print(f"An error occurred: {e}")
-            
-            pel = frappe.db.get_list("Payment Export Log", pluck="name")[0]
+    # for row in payment_type:
+        # data = get_payments(row, payment_export_settings)
+        # payments = data.get("payments")
+        # payments_ = []
+        # for d in payments:
+        #     payments_.append(d.get("name"))
+    if payments:
+        xml_content = generate_payment_file(payments, payment_export_settings, posting_date, payment_type, bank_account)
+        content = xml_content.get("content")
+        
+        file_name = "vesta/public/files/payments{0}.xml".format(xml_content.get("time"))
+        try:
+            with open(file_name, "w", encoding="utf-8") as file:
+                file.write(content)
+            frappe.msgprint(f"XML file '{file_name}' generated successfully!")
+        except Exception as e:
+            frappe.msgprint(f"An error occurred: {e}")
+        
+        pel = frappe.db.get_list("Payment Export Log", pluck="name")[0]
 
-            new_file = frappe.new_doc("File")
-            new_file.file_url = file_name.replace("vesta/public", '')
-            new_file.attached_to_doctype = "Payment Export Log"
-            new_file.attached_to_name = pel
-            new_file.save()
-            frappe.db.commit()
-            main_path = "/home/ubuntu/frappe-bench/sites/erpnext-skf-9150.frappe.cloud/public/"
-            file_url = get_attechment_paths(pel)
-            local_file = main_path + file_url
-            remote_path = "/in/payments/" 
-            sftp_instance = Sftp(
-                hostname=hostname,
-                username=username,
-                local_file=local_file,
-                remote_path=remote_path,
-                password=password,
-                port=port
-            )
-            sftp_instance.sftp_upload()
+        new_file = frappe.new_doc("File")
+        new_file.file_url = file_name.replace("vesta/public", '')
+        new_file.attached_to_doctype = "Payment Export Log"
+        new_file.attached_to_name = pel
+        new_file.save()
+        frappe.db.commit()
+        main_path = "/home/ubuntu/frappe-bench/sites/erpnext-skf-9150.frappe.cloud/public/"
+        file_url = get_attechment_paths(pel)
+        local_file = main_path + file_url
+        remote_path = "/in/payments/" 
+        sftp_instance = Sftp(
+            hostname=hostname,
+            username=username,
+            local_file=local_file,
+            remote_path=remote_path,
+            password=password,
+            port=port
+        )
+        sftp_instance.sftp_upload()
 
 
 
