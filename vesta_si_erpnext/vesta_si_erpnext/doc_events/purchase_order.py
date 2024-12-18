0cc1dc40f2769dde72b4ec55ed270a83eb920ac2
root <viral@fosserp.com>
2024-12-18 05:46:44 +0000

    ch

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_order.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_order.py
@@ -5,6 +5,9 @@ from vesta_si_erpnext.vesta_si_erpnext.doc_events.purchase_invoice import valida
 
 def validate(self , method):
     validate_currency(self)
+    if currency := frappe.db.get_value("Supplier", self.supplier, "default_currency"):
+        if currency != self.currency:
+            frappe.throw("Supplier base currency is {0}, Kindly create an invoice in <b>{0}</b>. Or Create new supplier with billing currency {1}".format(currency, self.currency))
     if not self.custom_level_1_approval_pending and self.workflow_state =="Level 1 Approval Pending":
         self.custom_level_1_approval_pending = now()
     if not self.custom_level_2_approval_pending and self.workflow_state == "Level 2 Approval Pending":
