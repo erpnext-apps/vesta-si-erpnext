9ea983d53c77f50c80af1c759196fb6fece64d64
viral <viral@fosserp.com>
2025-03-06 06:55:42 +0000

    Items Add Row button remove

--- vesta_si_erpnext/hooks.py
+++ vesta_si_erpnext/hooks.py
@@ -40,7 +40,8 @@ doctype_js = {
 	"Sales Invoice":"public/js/sales_invoice.js",
 	"Item" : "public/js/item.js",
 	"Purchase Invoice" : "public/js/purchase_invoice.js",
-	"Purchase Order" : "public/js/purchase_order.js"
+	"Purchase Order" : "public/js/purchase_order.js",
+	"Purchase Receipt" : "public/js/purchase_receipt.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
