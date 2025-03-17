9a1709f8be0403aca39df48e4bf16dc034b7c7a4
viral <viral@fosserp.com>
2025-03-17 13:25:18 +0000

    ch

--- vesta_si_erpnext/vesta_si_erpnext/report/project_budget_without_vat/project_budget_without_vat.js
+++ vesta_si_erpnext/vesta_si_erpnext/report/project_budget_without_vat/project_budget_without_vat.js
@@ -4,6 +4,23 @@
 
 frappe.query_reports["Project Budget without VAT"] = {
 	"filters": [
-
+		{
+			fieldname : "project",
+			label : "Project",
+			fieldtype : "Link",
+			options : "Project"
+   		},
+		{
+			fieldname : "status",
+			label : "PI Status",
+			fieldtype : "Select",
+			options : [
+				"",
+				"Paid",
+				"Partly Paid",
+				"Unpaid",
+				"Overdue",
+				]
+		}
 	]
 };
