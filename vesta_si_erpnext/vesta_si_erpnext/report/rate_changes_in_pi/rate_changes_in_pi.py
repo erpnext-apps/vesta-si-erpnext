8e87938ecc996840d6e607ee0da5903ba64d701e
viral <viral@fosserp.com>
2025-03-26 05:45:20 +0000

    Report Changes: fetch the old rate from pr

--- vesta_si_erpnext/vesta_si_erpnext/report/rate_changes_in_pi/rate_changes_in_pi.py
+++ vesta_si_erpnext/vesta_si_erpnext/report/rate_changes_in_pi/rate_changes_in_pi.py
@@ -43,13 +43,13 @@ def execute(filters=None):
 			"width" : 200
 		},
 		{
-			"fieldname" : "old_value",
+			"fieldname" : "old_rate",
 			"label" : "Old Value",
 			"fieldtype" : "Currency",
 			"width" : 200
 		},
 		{
-			"fieldname" : "currenct_value",
+			"fieldname" : "new_rate",
 			"label" : "Current Value",
 			"fieldtype" : "Currency",
 			"width" : 200
@@ -65,13 +65,6 @@ def execute(filters=None):
 			"label" : "Percentage",
 			"fieldtype" : "Percentage",
 			"width" : 200
-		},
-		{
-			"fieldname" : "user",
-			"label" : "User",
-			"fieldtype" : "Link",
-			"options" : "User",
-			"width" : 200
 		}
 	]
 	return columns, data
@@ -84,70 +77,21 @@ def get_data(filters):
 	if filters.get("to_date"):
 		cond += f" AND pi.posting_date <= '{filters.get('to_date')}'"
 
-	version = frappe.db.sql(f"""
-				SELECT 
-					vr.name, 
-					vr.data, 
-					vr.docname,
-					pi.name as purchase_invoice, pi.owner, 
-					pi.supplier,
-					pi.posting_date,
-					vr.owner
-				FROM 
-					`tabVersion` AS vr
-				LEFT JOIN 
-					`tabPurchase Invoice` AS pi 
-				ON 
-					vr.docname = pi.name
-				WHERE 
-					pi.docstatus = 1 
-					AND vr.ref_doctype = 'Purchase Invoice' {cond}
-			""", as_dict=1)
+	data = frappe.db.sql(f"""
+			Select pi.name as purchase_invoice,
+			pri.base_rate as old_rate,
+			pii.base_rate as new_rate,
+			(pii.base_rate - pri.base_rate) as difference,
+			ROUND(((pri.base_rate - pii.base_rate) * 100 / pii.base_rate), 2) as percentage,
+			pii.item_code,
+			pii.item_name,
+			pi.supplier,
+			pi.posting_date
+			From `tabPurchase Invoice Item` as pii 
+			Right Join `tabPurchase Receipt Item` as pri ON pri.name = pii.pr_detail
+			Left Join `tabPurchase Invoice` as pi ON pi.name = pii.parent
+			Where pii.docstatus = 1 and (pii.base_rate - pri.base_rate) != 0 {cond}
+	""", as_dict=1 )
 
-	purchase_invoice = frappe.db.sql(f"""
-			SELECT 
-				pi.name,
-				pii.item_code,
-				pii.item_name,
-				pii.idx
-			From `tabPurchase Invoice` as pi
-			Left Join `tabPurchase Invoice Item` as pii ON pi.name = pii.parent
-			Where pi.docstatus = 1 {cond}
-	""", as_dict=1)
-	pi_map = {}
-	for row in purchase_invoice:
-		if pi_map.get(row.name):
-			pi_map[row.name].append(row)
-		else:
-			pi_map[row.name] = [row]
-	value = []
-	for row in version:
-		if "row_changed" in row.data and "base_rate" in row.data:
-			data = json.loads(row.data)
-			if data.get("row_changed"):
-				if data.get("row_changed")[0][0] == "items":
-					for d in data.get("row_changed")[0][-1]:
-						if d[0] == "base_rate":
-							print(d)
-							old_value = d[-2].replace("SEK", "").replace(",",".").replace(" " ,"") if not d[-2] == 0.0 else 0
-							currenct_value =d[-1].replace("SEK", "").replace(",",".").replace(" " ,"") if not d[-1] == 0.0 else 0
-							value.append({
-								"purchase_invoice" : row.get("purchase_invoice"),
-								"posting_date" : row.get("posting_date"),
-								"user" : row.get("owner"),
-								"supplier" : row.get("supplier"),
-								"old_value" : flt(old_value),
-								"currenct_value" : flt(currenct_value),
-								"difference" : flt(currenct_value) - flt(old_value),
-								"percentage" : round((flt(currenct_value) - flt(old_value)) * 100 / flt(old_value), 2) if old_value !=0 else 0,
-								"idx" : data.get("row_changed")[0][1],
- 							})
-	for row in value:
-		if pi_map.get(row.get("purchase_invoice")):
-			for d in pi_map.get(row.get("purchase_invoice")):
-				if (d.get("idx")) - 1 == row.get("idx"):
-					row.update({
-						"item_code" : d.get("item_code"),
-						"item_name" : d.get("item_name")
-					})
-	return value
+
+	return data
