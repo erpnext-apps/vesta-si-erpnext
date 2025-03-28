4d56fe91a08a7a1e825bbd339f3a27ccede0eb83
viral <viral@fosserp.com>
2025-03-28 06:00:16 +0000

    New report project budget from parent level project

---
+++ vesta_si_erpnext/vesta_si_erpnext/report/project_budget_without_vat___(parent_level)/project_budget_without_vat___(parent_level).js
@@ -0,0 +1,26 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Project Budget without VAT - (Parent Level)"] = {
+	"filters": [
+		{
+			fieldname : "project",
+			label : "Project",
+			fieldtype : "Link",
+			options : "Project"
+   		},
+		{
+			fieldname : "status",
+			label : "PI Status",
+			fieldtype : "Select",
+			options : [
+				"",
+				"Paid",
+				"Partly Paid",
+				"Unpaid",
+				"Overdue",
+				]
+		}
+	]
+};
