b0f957227bac21e6e7d3fba1b9908774fc04e83e
viral <viral@fosserp.com>
2025-04-10 07:14:09 +0000

    debug import to check

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/xml_automation.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/xml_automation.py
@@ -49,7 +49,6 @@ def get_payment_entry(payments, payment_export_settings, posting_date, payment_t
             main_path = "/home/frappe/frappe-bench/sites/{0}".format(url)
             file_url = get_attechment_paths(pel)
             local_file = os.path.join(main_path, file_url[1:])
-            frappe.throw(str(local_file))
             remote_path = "/in/payments/" 
             sftp_instance = Sftp(
                 hostname=hostname,
