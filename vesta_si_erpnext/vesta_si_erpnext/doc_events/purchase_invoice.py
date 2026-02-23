eeedeb5c37ef2c097558ac00488bdbaedde5bb9c
ViralKansodiya-Fosserp <141210323+viralkansodiya@users.noreply.github.com>
2026-02-23 17:00:22 +0530

    Refactor purchase invoice validation logic

--- vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
+++ vesta_si_erpnext/vesta_si_erpnext/doc_events/purchase_invoice.py
@@ -26,57 +26,95 @@ def set_due_date_after_submit(self, method):
             self.reload()
 
 def validate(self, method):
-    get_advance_entries(self)
+	get_advance_entries(self)
 
-    party_account_currency = frappe.db.get_value("Account", self.credit_to, 'account_currency')
-    company_currency = frappe.db.get_value("Company", self.company, 'default_currency')
-    
-    data = get_negative_outstanding_invoices(
-                "Supplier", 
-                self.supplier, 
-                self.credit_to, 
-                party_account_currency, 
-                company_currency,
-                condition = '')
-                
-    # frappe.throw(str(data))
+	party_account_currency = frappe.db.get_value("Account", self.credit_to, 'account_currency')
+	company_currency = frappe.db.get_value("Company", self.company, 'default_currency')
+	
+	data = get_negative_outstanding_invoices(
+				"Supplier", 
+				self.supplier, 
+				self.credit_to, 
+				party_account_currency, 
+				company_currency,
+				condition = '')
+				
+	# frappe.throw(str(data))
 
-    final_data = []
-    for row in data:
-        if not row.voucher_no in ["ACC-PINV-2024-00250-1", "ACC-PINV-2024-00251-1"]:
-            final_data.append(row)
-    # if len(final_data):
-    # 	message = "Debit Note and Payment Entry available against this supplier <b>{0}</b><br>".format(get_link_to_form("Supplier",self.supplier))
-    # 	message +="First reconcile those entry, reference available as mentioned below"
-    # 	message += "<br><br>"
-    # 	message += """<table width='100%'>"""
-    # 	for row in data:
-    # 		message += "<tr><td>{0}</td><td>{1} {2}</td></tr>".format(get_link_to_form(row.voucher_type, row.voucher_no),self.currency, row.outstanding_amount)
-    # 	message += "</table>"
-        
-    # 	frappe.msgprint(message)
-        
-    if not self.is_return:
-        set_exchange_rate(self, method)
-        self.validate()
-    check_item_level_changes(self) 
-    validate_currency(self) #Do not deploy
+	final_data = []
+	for row in data:
+		if not row.voucher_no in ["ACC-PINV-2024-00250-1", "ACC-PINV-2024-00251-1"]:
+			final_data.append(row)
+	# if len(final_data):
+	# 	message = "Debit Note and Payment Entry available against this supplier <b>{0}</b><br>".format(get_link_to_form("Supplier",self.supplier))
+	# 	message +="First reconcile those entry, reference available as mentioned below"
+	# 	message += "<br><br>"
+	# 	message += """<table width='100%'>"""
+	# 	for row in data:
+	# 		message += "<tr><td>{0}</td><td>{1} {2}</td></tr>".format(get_link_to_form(row.voucher_type, row.voucher_no),self.currency, row.outstanding_amount)
+	# 	message += "</table>"
+		
+	# 	frappe.msgprint(message)
+		
+	if not self.is_return:
+		set_exchange_rate(self, method)
+		self.validate()
+		check_item_level_changes(self) 
+	validate_currency(self) #Do not deploy
 
 def after_insert(self, method):
     get_attachment_from_po(self)
 
+import os
+import frappe
+from frappe.utils import get_site_path
+
+
+def file_exists_on_disk(file_url):
+	if not file_url:
+		return False
+
+	file_url = file_url.lstrip("/")
+	file_path = get_site_path(file_url)
+	return os.path.exists(file_path)
+
+
 def get_attachment_from_po(self):
-    if self.items[0].get("purchase_receipt"):
-        attached_files = get_attachments("Purchase Receipt", self.items[0].get("purchase_receipt"))
-        for row in attached_files:
-            new_file = frappe.get_doc({ 
-                "doctype" : "File",
-                "file_name" : row.file_name,
-                "file_url" : row.file_url,
-                "attached_to_doctype" : "Purchase Invoice",
-                "attached_to_name" : self.name
-            })
-            new_file.insert(ignore_permissions=True)
+	if not self.items or not self.items[0].get("purchase_receipt"):
+		return
+
+	attached_files = get_attachments(
+		"Purchase Receipt",
+		self.items[0].get("purchase_receipt")
+	)
+
+	for file_row in attached_files:
+		# 1️⃣ Check if already attached to this Purchase Invoice
+		exists_in_pi = frappe.db.exists(
+			"File",
+			{
+				"file_url": file_row.file_url,
+				"attached_to_doctype": "Purchase Invoice",
+				"attached_to_name": self.name,
+			},
+		)
+
+		# 2️⃣ If record exists AND file exists physically → skip
+		if exists_in_pi and file_exists_on_disk(file_row.file_url):
+			continue
+
+		try:
+			new_file = frappe.get_doc({
+				"doctype": "File",
+				"file_name": file_row.file_name,
+				"file_url": file_row.file_url,
+				"attached_to_doctype": "Purchase Invoice",
+				"attached_to_name": self.name,
+			})
+			new_file.insert(ignore_permissions=True)
+		except:
+			frappe.log_error("File is not exists", self.name)
+
 
 
 def get_advance_entries(self):
@@ -163,24 +201,41 @@ def get_advance_entries_w(self):
 
 # Not allow to change item code , qty and not allow to add row
 def check_item_level_changes(self):
-    buying_setting =  frappe.get_doc("Buying Settings")
-    allow_extra_item =[ row.item for row in buying_setting.allow_extra_item]
-    po_flage = False
-    for row in self.items:
-        if row.purchase_receipt:
-            po_flage = True
-            po_data = frappe.db.sql(f"""
-                        Select item_code, name, qty, base_amount
-                        From `tabPurchase Order Item`
-                        Where name = '{row.po_detail}'
-            """, as_dict = 1)
-            item_allownce = frappe.db.get_value("Item", row.item_code, "overbill_allow_by_amount")
-        
-            if not item_allownce and not self.is_new() and (row.base_amount - po_data[0].get("base_amount")) > item_allownce and self.currency == "SEK":
-                frappe.throw(f"Row #{row.idx} : Overbilling not allow for Item <b>{row.item_code}</b>")
-            if row.qty != po_data[0].get("qty") and frappe.db.get_value("Item", row.item_code, "is_stock_item"):
-                frappe.throw(f"Row #{row.idx} : Accepted Qty should be same as purchase receipt quantiy")
-            if item_allownce > 0  and (row.base_amount - po_data[0].get("base_amount")) > item_allownce and self.currency == "SEK":
-                frappe.throw(f"Row #{row.idx} : Overbilling is not allowed beyond {item_allownce} SEK.")
-        if po_flage and not row.purchase_order and row.item_code not in allow_extra_item:
-            frappe.throw(f"Row #{row.idx}: Not Allow to add Item, It should be available in purchase order")
+	buying_setting =  frappe.get_doc("Buying Settings")
+	allow_extra_item =[ row.item for row in buying_setting.allow_extra_item]
+	po_flage = False
+	for row in self.items:
+		if row.purchase_receipt:
+			po_flage = True
+			po_data = frappe.db.sql(f"""
+						Select item_code, name, qty, base_amount
+						From `tabPurchase Order Item`
+						Where name = '{row.po_detail}'
+			""", as_dict = 1)
+			if not po_data:
+				continue
+			item_allownce_percentage = 0
+			item_allownce =0
+
+			allow_overbill_by = frappe.db.get_value("Item", row.item_code, "allow_overbill_by")
+			if (allow_overbill_by == "Don't Allow Overbilling") and row.base_amount > po_data[0].get("base_amount") and self.currency == "SEK":
+				frappe.throw(f"Row #{row.idx} : Not allow to change a rate for item <b>{row.item_code}</b>")
+
+			if allow_overbill_by == "Amount":
+				item_allownce = frappe.db.get_value("Item", row.item_code, "overbill_allow_by_amount")
+			if allow_overbill_by == "Percentage":
+				item_allownce_percentage = frappe.db.get_value("Item", row.item_code, "over_billing_allowance")
+				if item_allownce_percentage:
+					item_allownce = po_data[0].get("base_amount") * flt(item_allownce_percentage) / 100
+			
+			if not item_allownce and not self.is_new() and (row.base_amount - po_data[0].get("base_amount")) > item_allownce and self.currency == "SEK":
+				frappe.throw(f"Row #{row.idx} : Overbilling not allow for Item <b>{row.item_code}</b>")
+			# if row.qty != po_data[0].get("qty") and frappe.db.get_value("Item", row.item_code, "is_stock_item"):
+			# 	frappe.throw(f"Row #{row.idx} : Accepted Qty should be same as purchase receipt quantiy")
+			if item_allownce > 0  and (row.base_amount - po_data[0].get("base_amount")) > item_allownce and self.currency == "SEK":
+				if not item_allownce_percentage:
+					frappe.throw(f"Row #{row.idx} : Overbilling is not allowed beyond {item_allownce} SEK.")
+				else:
+					frappe.throw(f"Row #{row.idx} : Overbilling is not allowed beyond {item_allownce} SEK ({item_allownce_percentage} %) .")
+		if po_flage and not row.purchase_order and row.item_code not in allow_extra_item:
+			frappe.throw(f"Row #{row.idx}: Not Allow to add Item, It should be available in purchase order")
